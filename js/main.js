a5edab71190758afa74ec10764609e8f3d8a6b80
Volodymyr-Horbach <103590333+Volodymyr-Horbach@users.noreply.github.com>
2022-05-20 13:50:07 +0300

    burger_js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
+// swiper 
 const swiper = new Swiper('.reviews__slider', {
     loop: true,
     pagination: {
       el: '.reviews-pagination',
     },
 });
+
+// burger
+let burger = document.querySelector('.header__burger')
+let menu = document.querySelector('.header__information')
+
+document.addEventListener("DOMContentLoaded",function(){
+  document.querySelector('.header__burger').onclick = function(){
+      menu.classList.toggle('active')
+      burger.classList.toggle('active');
+      document.querySelector('body').classList.toggle('lock')
+  }
+})
