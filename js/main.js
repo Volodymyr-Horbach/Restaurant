cec24f8c27affb66f3bff9c6d0d32c5d781b0d57
Volodymyr-Horbach <103590333+Volodymyr-Horbach@users.noreply.github.com>
2022-05-21 13:20:26 +0300

    Popup

--- js/main.js
+++ js/main.js
@@ -18,3 +18,35 @@ document.addEventListener("DOMContentLoaded",function(){
       document.querySelector('body').classList.toggle('lock')
   }
 })
+
+// popop
+let popupBg = document.querySelector('.popup__bg')
+let popup = document.querySelector('.popup')
+let openPopunButton = document.querySelectorAll('.open-popup')
+let closePopupButton = document.querySelector('.close-popup')
+
+openPopunButton.forEach((buttom)=> {
+  buttom.addEventListener('click', (e)=>{
+    e.preventDefault()
+    popupBg.classList.add('active')
+    popup.classList.add('active')
+  })
+});
+
+closePopupButton.addEventListener('click', ()=>{
+  popupBg.classList.remove('active')
+  popup.classList.remove('active')
+})
+
+document.addEventListener('click', (e)=>{
+  if(e.target === popupBg){
+    popupBg.classList.remove('active');
+    popup.classList.remove('active')
+  }
+})
+
+// let popupBtn = document.querySelector('.popup__btn')
+
+// popupBtn.addEventListener('click', ()=>{
+//   console.log('fff')
+// })
